0a95d05d7a59d1e722ea27a486975c8d3a7848ee
exceldeo <excel.deo99@gmail.com>
2022-01-13 23:51:06 +0700

    rowcolumn

---
+++ my_app/lib/RowColumnScreen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class RowColumnScreen extends StatefulWidget {
+  RowColumnScreen({Key key}) : super(key: key);
+
+  @override
+  _RowColumnScreenState createState() => _RowColumnScreenState();
+}
+
+class _RowColumnScreenState extends State<RowColumnScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(children: [
+              Text("Hello World"),
+              Text("Hello World"),
+              Text("Hello World"),
+              Text("Hello World"),
+            ]),
+            SizedBox(
+              height: 13,
+            ),
+            Row(children: [
+              Text("Hello World"),
+              Text("Hello World"),
+              Text("Hello World"),
+              Text("Hello World"),
+            ]),
+            SizedBox(
+              height: 13,
+            ),
+            Row(children: [
+              Text("Hello World"),
+              Text("Hello World"),
+              Text("Hello World"),
+              Text("Hello World"),
+            ]),
+          ],
+        ),
+      ),
+    );
+  }
+}
