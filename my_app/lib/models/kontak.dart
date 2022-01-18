52d046f6fd73172a6763fea9b51d4b3a6b6de92a
exceldeo <excel.deo99@gmail.com>
2022-01-18 23:52:12 +0700

    memperbaiki kontak

--- my_app/lib/models/kontak.dart
+++ my_app/lib/models/kontak.dart
@@ -1,7 +1,26 @@
 class KontakModel {
+  static const tblKontak = 'kontak';
+  static const colId = 'id';
+  static const colNama = 'nama';
+  static const colnoHp = 'noHp';
+
   int id;
   String nama;
   String noHp;
 
   KontakModel({this.id, this.nama, this.noHp});
+
+  KontakModel.fromMap(Map<String, dynamic> map) {
+    id = map[colId];
+    nama = map[colNama];
+    noHp = map[colnoHp];
+  }
+
+  Map<String, dynamic> toMap() {
+    var map = <String, dynamic>{colNama: nama, colnoHp: noHp};
+    if (id != null) {
+      map[colId] = id;
+    }
+    return map;
+  }
 }
